19eb1fcfb3cce9f552d8f8a92cfe5d0f5b110d9b
Churexiz <104880287+Churexiz@users.noreply.github.com>
2022-05-03 23:16:04 -0500

    Update frmDocente.xaml.cs

--- frmDocente.xaml.cs
+++ frmDocente.xaml.cs
@@ -65,3 +65,44 @@ namespace CapaPresentacion.Mantenimiento
         }
     }
 }
+//CODIGO DE LOS ITEMS DE LA VENTANA
+<Window x:Class="CapaPresentacion.Mantenimiento.frmDocente"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:CapaPresentacion.Mantenimiento"
+        mc:Ignorable="d"
+        Title="frmDocente" Height="450" Width="800">
+    <Grid>
+        <TextBox x:Name="txtNombres" HorizontalAlignment="Left" Height="26" Margin="254,193,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <TextBox x:Name="txtApellidos" HorizontalAlignment="Left" Height="26" Margin="254,162,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <TextBox x:Name="txtDomicilio" HorizontalAlignment="Left" Height="26" Margin="254,224,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <TextBox x:Name="txtCorreo" HorizontalAlignment="Left" Height="26" Margin="254,255,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <ComboBox x:Name="cbxLugarNac" HorizontalAlignment="Left" Margin="254,286,0,0" VerticalAlignment="Top" Width="120" Height="26">
+            <TextBlock HorizontalAlignment="Left" TextWrapping="Wrap" Text="Arequipa" VerticalAlignment="Top"/>
+            <TextBlock HorizontalAlignment="Left" TextWrapping="Wrap" Text="Cusco" VerticalAlignment="Top"/>
+            <TextBlock HorizontalAlignment="Left" TextWrapping="Wrap" Text="Lima" VerticalAlignment="Top"/>
+            <TextBlock HorizontalAlignment="Left" TextWrapping="Wrap" Text="Madre de Dios" VerticalAlignment="Top"/>
+            <TextBlock HorizontalAlignment="Left" TextWrapping="Wrap" Text="Puno" VerticalAlignment="Top"/>
+        </ComboBox>
+        <DatePicker x:Name="dpFechaNac" HorizontalAlignment="Left" Margin="254,317,0,0" VerticalAlignment="Top" Height="26" Width="145"/>
+        <TextBox x:Name="txtCodigo" HorizontalAlignment="Left" Height="26" Margin="254,131,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <Button x:Name="btnGuardar" Content="Guardar" HorizontalAlignment="Left" Margin="479,209,0,0" VerticalAlignment="Top" Width="75" Height="20" Click="btnGuardar_Click" Background="#FF1C597C" FontWeight="Bold" BorderBrush="{x:Null}" Foreground="{DynamicResource {x:Static SystemColors.ControlLightLightBrushKey}}"/>
+        <Button x:Name="btnMostrar" Content="Mostrar" HorizontalAlignment="Left" Margin="479,254,0,0" VerticalAlignment="Top" Width="75" Height="20" Click="btnMostrar_Click" Background="#FF1C597C" FontWeight="Bold" BorderBrush="{x:Null}" Foreground="{DynamicResource {x:Static SystemColors.ControlLightLightBrushKey}}"/>
+        <Button x:Name="btnMetodo1" Content="Método 1" HorizontalAlignment="Left" Margin="608,209,0,0" VerticalAlignment="Top" Width="75" Height="20" Click="btnMetodo1_Click" BorderBrush="{x:Null}" Background="#FFE65E5E" Foreground="{DynamicResource {x:Static SystemColors.ControlLightLightBrushKey}}" FontWeight="Bold"/>
+        <TextBox x:Name="txtProfesion" HorizontalAlignment="Left" Height="26" Margin="254,348,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
+        <Label Content="Código:" HorizontalAlignment="Left" Margin="118,131,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Label Content="Apellidos:" HorizontalAlignment="Left" Margin="118,162,0,0" VerticalAlignment="Top" RenderTransformOrigin="0.5,0.346" FontWeight="Bold"/>
+        <Label Content="Nombres:" HorizontalAlignment="Left" Margin="118,193,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Label Content="Domicilio" HorizontalAlignment="Left" Margin="118,224,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Label Content="Correo:" HorizontalAlignment="Left" Margin="118,255,0,0" VerticalAlignment="Top" RenderTransformOrigin="0.694,0.615" FontWeight="Bold"/>
+        <Label Content="Lugar de Nacimiento:" HorizontalAlignment="Left" Margin="118,286,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Label Content="Fecha de Nacimiento:" HorizontalAlignment="Left" Margin="118,317,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Label Content="Profesión:" HorizontalAlignment="Left" Margin="118,348,0,0" VerticalAlignment="Top" FontWeight="Bold"/>
+        <Rectangle Fill="#FF264078" HorizontalAlignment="Left" Height="37" VerticalAlignment="Top" Width="792"/>
+        <Label Content="SISTEMA ACADÉMICO UAC" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" FontWeight="Bold" Background="{x:Null}" Foreground="{DynamicResource {x:Static SystemColors.ControlLightLightBrushKey}}"/>
+        <Image Margin="311,42,332,316" Source="/CapaPresentacion;component/UACazul.png" Stretch="Fill"/>
+
+    </Grid>
+</Window>
